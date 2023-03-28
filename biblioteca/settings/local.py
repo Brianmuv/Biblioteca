1a4cda4326523b31990b0c0a0fa222603ba5d400
Brianmuv <brian200698@gmail.com>
2023-03-28 16:39:13 -0500

    Started project

---
+++ biblioteca/settings/local.py
@@ -0,0 +1,22 @@
+from .base import *
+# SECURITY WARNING: don't run with debung tuner on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbbiblioteca',
+        'USER' : 'Brianmuv',
+        'PASSWORD' : 'Brianmuv2023',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
